363070eaaaa9672cb68125b2129873d3dd2f45c0
sanjayamal <rajithasanjayamal0918@gmail.com>
2024-01-17 21:24:20 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,11 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
-    "api/webhook/clerk",
-    "api/webhook/stripe",
-    "api/uploading",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploading",
   ],
-  ignoredRoutes: ["api/webhook/clerk", "api/webhook/stripe", "api/uploading"],
+  ignoredRoutes: ["/api/webhook/clerk", "/api/webhook/stripe", "/api/uploading"],
 });
 
 export const config = {
